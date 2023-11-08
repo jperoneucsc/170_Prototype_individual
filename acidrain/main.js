3584427a014186d12c2cf05d55b4ec5aa86b1ee2
Arya Miryala <aryamiryala5@gmail.com>
2023-11-08 15:00:01 -0800

    acidrain prototype

---
+++ acidrain/main.js
@@ -0,0 +1,27 @@
+title = "Acid Rain";
+
+description = `
+[TAP] Switch
+
+`;
+
+characters = [
+
+`
+   ll
+   l  
+lbbbb
+ rp  l
+r  r
+    r
+`
+,
+ ];
+
+options = {};
+
+function update() {
+  if (!ticks) {
+  }
+  char("a", 30, 30)
+}
