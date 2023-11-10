7006d0da190c4ae8b5a1d03788fc4e90274dc68c
John Perone <132640078+jperoneucsc@users.noreply.github.com>
2023-11-09 19:43:57 -0800

    Added Player sprite and basic movement

--- acidrain/main.js
+++ acidrain/main.js
@@ -2,75 +2,131 @@ title = "Acid Rain";
 
 description = `
 [TAP] Switch
-
 `;
 
 characters = [
-
-`
-   ll
-   l  
-lbbbb
- rp  l
-r  r
+  `
+  llll
+  llll
+   r
+  rrr
+   ccc
+  c
+  `,
+  `
+  llll
+  llll
     r
-`
-,
- ];
+   rrr
+  ccc
+    c
+  `,
+];
 
 const G = {
-	WIDTH: 70,
-	HEIGHT: 100,
+  WIDTH: 70,
+  HEIGHT: 100,
   raindrops_SPEED_MIN: 0.5,
-	raindrops_SPEED_MAX: 1.0
+  raindrops_SPEED_MAX: 1.0,
+  player_speed: 0.5,
+  animationDelay: 10, // Adjust the delay value as needed
 };
 
 options = {
-  viewSize: {x: G.WIDTH, y: G.HEIGHT}
+  viewSize: { x: G.WIDTH, y: G.HEIGHT },
 };
 
 /**
-* @typedef {{
-  * pos: Vector,
-  * speed: number
-  * }} Raindrop
-  */
-  
-  /**
-  * @type  { Raindrop [] }
-  */
-  let raindrops;
+ * @typedef {{
+ * pos: Vector,
+ * frameIndex: number
+ * }} Player
+ */
 
+/**
+ * @type  { Player }
+ */
+let player;
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * speed: number
+ * }} Raindrop
+ */
+
+/**
+ * @type  { Raindrop [] }
+ */
+let raindrops;
+
+// Direction variable for the player
+let playerDirection = 1; // 1 for right, -1 for left
+
+// Animation frames for the player
+const playerFrames = [0, 1];
+let playerFrameIndex = 0;
+
+// Counter for animation delay
+let animationCounter = 0;
 
 // The game loop function
 function update() {
   // The init function running at startup
-      if (!ticks) {
-              raindrops = times(6, () => {
-                // Random number generator function
-                // rnd( min, max )
-              const posX = rnd(0, G.WIDTH);
-              const posY = rnd(0, G.HEIGHT);
-                // An object of type raindrop with appropriate properties
-              return {
-                  // Creates a Vector
-                    pos: vec(posX, posY),
-                    // More RNG
-                    speed: rnd(G.raindrops_SPEED_MIN, G.raindrops_SPEED_MAX)
-              };
-          });
-      } //end of if !ticks
-      // Update for raindrop
-      raindrops.forEach((r) => {
-        // Move the raindrop downwards
-        r.pos.y += r.speed;
-        // Bring the raindrop back to top once it's past the bottom of the screen
-        r.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+  if (!ticks) {
+    raindrops = times(6, () => {
+      const posX = rnd(0, G.WIDTH);
+      const posY = rnd(0, G.HEIGHT);
+      return {
+        pos: vec(posX, posY),
+        speed: rnd(G.raindrops_SPEED_MIN, G.raindrops_SPEED_MAX),
+      };
+    });
 
-        // Choose a color to draw
-        color("blue");
-        // Draw the raindrop as a square of size 1
-        box(r.pos, 1.3);
-      });
+    player = {
+      pos: vec(G.WIDTH * 0.5, 94),
+      frameIndex: 0,
+    };
+  } // end of if !ticks
+
+  // Draw rectangles for border
+  color("black");
+  rect(0, 0, 1, 99); // left border
+  rect(69, 0, 1, 99); // right border
+  rect(0, 97, 99, 3); // floor border
+
+  // PLAYER ----------------
+  player.pos.x += G.player_speed * playerDirection;
+  // When player taps switch direction
+  if (input.isJustPressed) {
+    playerDirection *= -1;
+  }
+  // Ensure the player stays within the game boundaries
+  player.pos.x = clamp(player.pos.x, 2, 67);
+  if(player.pos.x == 2 || player.pos.x == 67)
+  {
+    playerDirection *= -1;
+  }
+  // Draw the player
+  color("black");
+
+  // Update player animation frame with delay
+  if (animationCounter % G.animationDelay === 0) {
+    playerFrameIndex = (playerFrameIndex + 1) % playerFrames.length;
+  }
+  animationCounter++;
+
+  if (playerFrameIndex === 0) {
+    char("a", player.pos);
+  } else if (playerFrameIndex === 1) {
+    char("b", player.pos);
+  }
+
+  // Update for raindrop
+  raindrops.forEach((r) => {
+    r.pos.y += r.speed;
+    r.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+    color("blue");
+    box(r.pos, 1.3);
+  });
 }
-
